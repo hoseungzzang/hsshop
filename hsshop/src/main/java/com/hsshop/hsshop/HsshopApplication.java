36e36b3f791aee0246bcec270240e2e7199bfc6f
hoseungzzang <wnghtmd99@naver.com>
2022-11-06 23:49:39 +0900

    환경세팅완료

---
+++ hsshop/src/main/java/com/hsshop/hsshop/HsshopApplication.java
@@ -0,0 +1,13 @@
+package com.hsshop.hsshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsshopApplication.class, args);
+	}
+
+}
